10e288fe340e52745f43feeb3303b01fac56f7f2
daudikombe <daudikombe77@gmail.com>
2026-04-15 12:01:26 +0300

    complete analyzing data modules syntax js

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+//creating a function to combine users
+function combineUsers(...args) {
+//initialise retuned object
+  const combinedObject = {
+    users: []
+  };
+  //loop through args
+  for (const arr of args) {
+    combinedObject.users.push(...arr);
+  }
+  //add todays date
+  const today = new Date();
+  combinedObject.merge_date= today.toString('M/d/yyyy');
 
+return combinedObject;
+}
 
 
 module.exports = {
